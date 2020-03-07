4011f10dc51b0a5e70260b83db3d08eea68c610f
Anton Miselaytes <5480808+miselaytes-anton@users.noreply.github.com>
2020-03-07 22:31:02 +0100

    Format pumpkin files

--- examples/Sensors/pumpkin/I2C_MPR121.cpp
+++ examples/Sensors/pumpkin/I2C_MPR121.cpp
@@ -5,35 +5,30 @@
  *      Author: Victor Zappi
  */
 
-
 #include "I2C_MPR121.h"
 #include <iostream>
 
-I2C_MPR121::I2C_MPR121() {
-
-}
+I2C_MPR121::I2C_MPR121() {}
 
 boolean I2C_MPR121::begin(uint8_t bus, uint8_t i2caddr) {
   _i2c_address = i2caddr;
-	
-  if(initI2C_RW(bus, i2caddr, 0) > 0)
-	  return false;
+
+  if (initI2C_RW(bus, i2caddr, 0) > 0) return false;
 
   // soft reset
   writeRegister(MPR121_SOFTRESET, 0x63);
   usleep(1000);
   // for (uint8_t i=0; i<0x7F; i++) {
-  //  rt_printf("%f\n", readRegister8(i)); 
+  //  rt_printf("%f\n", readRegister8(i));
   // }
-  
 
   writeRegister(MPR121_ECR, 0x0);
 
   uint8_t c = readRegister8(MPR121_CONFIG2);
-  
+
   if (c != 0x24) {
-	  rt_printf("MPR121 read 0x%x instead of 0x24\n", c);
-	  return false;
+    rt_printf("MPR121 read 0x%x instead of 0x24\n", c);
+    return false;
   }
 
   setThresholds(12, 6);
@@ -52,110 +47,113 @@ boolean I2C_MPR121::begin(uint8_t bus, uint8_t i2caddr) {
   writeRegister(MPR121_FDLT, 0x00);
 
   writeRegister(MPR121_DEBOUNCE, 0);
-  writeRegister(MPR121_CONFIG1, 0x10); // default, 16uA charge current
-  writeRegister(MPR121_CONFIG2, 0x20); // 0.5uS encoding, 1ms period
+  // default, 16uA charge current
+  writeRegister(MPR121_CONFIG1, 0x10);
+  // 0.5uS encoding, 1ms period
+  writeRegister(MPR121_CONFIG2, 0x20);
 
-//  writeRegister(MPR121_AUTOCONFIG0, 0x8F);
-
-//  writeRegister(MPR121_UPLIMIT, 150);
-//  writeRegister(MPR121_TARGETLIMIT, 100); // should be ~400 (100 shifted)
-//  writeRegister(MPR121_LOWLIMIT, 50);
   // enable all electrodes
-  writeRegister(MPR121_ECR, 0x8F);  // start with first 5 bits of baseline tracking
+  // start with first 5 bits of baseline tracking
+  writeRegister(MPR121_ECR, 0x8F);
 
   return true;
 }
 
 void I2C_MPR121::setThresholds(uint8_t touch, uint8_t release) {
-  for (uint8_t i=0; i<12; i++) {
-    writeRegister(MPR121_TOUCHTH_0 + 2*i, touch);
-    writeRegister(MPR121_RELEASETH_0 + 2*i, release);
+  for (uint8_t i = 0; i < 12; i++) {
+    writeRegister(MPR121_TOUCHTH_0 + 2 * i, touch);
+    writeRegister(MPR121_RELEASETH_0 + 2 * i, release);
   }
 }
 
-uint16_t  I2C_MPR121::filteredData(uint8_t t) {
+uint16_t I2C_MPR121::filteredData(uint8_t t) {
   if (t > 12) return 0;
-  return readRegister16(MPR121_FILTDATA_0L + t*2);
+  return readRegister16(MPR121_FILTDATA_0L + t * 2);
 }
 
-uint16_t  I2C_MPR121::baselineData(uint8_t t) {
+uint16_t I2C_MPR121::baselineData(uint8_t t) {
   if (t > 12) return 0;
   uint16_t bl = readRegister8(MPR121_BASELINE_0 + t);
   return (bl << 2);
 }
 
-uint16_t  I2C_MPR121::touched(void) {
+uint16_t I2C_MPR121::touched(void) {
   uint16_t t = readRegister16(MPR121_TOUCHSTATUS_L);
   return t & 0x0FFF;
 }
 
+int I2C_MPR121::getSensorValue(uint8_t t, uint16_t threshold) {
+  if (t > 12) return 0;
+  int v = -(filteredData(t) - baselineData(t)) - threshold;
+  return v < 0 ? 0 : v;
+}
+
 /*********************************************************************/
 
-
 uint8_t I2C_MPR121::readRegister8(uint8_t reg) {
-    i2c_char_t inbuf, outbuf;
-    struct i2c_rdwr_ioctl_data packets;
-    struct i2c_msg messages[2];
+  i2c_char_t inbuf, outbuf;
+  struct i2c_rdwr_ioctl_data packets;
+  struct i2c_msg messages[2];
 
-    /*
-     * In order to read a register, we first do a "dummy write" by writing
-     * 0 bytes to the register we want to read from.  This is similar to
-     * the packet in set_i2c_register, except it's 1 byte rather than 2.
-     */
-    outbuf = reg;
-    messages[0].addr  = _i2c_address;
-    messages[0].flags = 0;
-    messages[0].len   = sizeof(outbuf);
-    messages[0].buf   = &outbuf;
+  /*
+   * In order to read a register, we first do a "dummy write" by writing
+   * 0 bytes to the register we want to read from.  This is similar to
+   * the packet in set_i2c_register, except it's 1 byte rather than 2.
+   */
+  outbuf = reg;
+  messages[0].addr = _i2c_address;
+  messages[0].flags = 0;
+  messages[0].len = sizeof(outbuf);
+  messages[0].buf = &outbuf;
 
-    /* The data will get returned in this structure */
-    messages[1].addr  = _i2c_address;
-    messages[1].flags = I2C_M_RD/* | I2C_M_NOSTART*/;
-    messages[1].len   = sizeof(inbuf);
-    messages[1].buf   = &inbuf;
+  /* The data will get returned in this structure */
+  messages[1].addr = _i2c_address;
+  messages[1].flags = I2C_M_RD /* | I2C_M_NOSTART*/;
+  messages[1].len = sizeof(inbuf);
+  messages[1].buf = &inbuf;
 
-    /* Send the request to the kernel and get the result back */
-    packets.msgs      = messages;
-    packets.nmsgs     = 2;
-    if(ioctl(i2C_file, I2C_RDWR, &packets) < 0) {
-        rt_printf("Unable to send data");
-        return 0;
-    }
+  /* Send the request to the kernel and get the result back */
+  packets.msgs = messages;
+  packets.nmsgs = 2;
+  if (ioctl(i2C_file, I2C_RDWR, &packets) < 0) {
+    rt_printf("Unable to send data");
+    return 0;
+  }
 
-    return inbuf;
+  return inbuf;
 }
 
 uint16_t I2C_MPR121::readRegister16(uint8_t reg) {
-    i2c_char_t inbuf[2], outbuf;
-    struct i2c_rdwr_ioctl_data packets;
-    struct i2c_msg messages[2];
+  i2c_char_t inbuf[2], outbuf;
+  struct i2c_rdwr_ioctl_data packets;
+  struct i2c_msg messages[2];
 
-    /*
-     * In order to read a register, we first do a "dummy write" by writing
-     * 0 bytes to the register we want to read from.  This is similar to
-     * the packet in set_i2c_register, except it's 1 byte rather than 2.
-     */
-    outbuf = reg;
-    messages[0].addr  = _i2c_address;
-    messages[0].flags = 0;
-    messages[0].len   = sizeof(outbuf);
-    messages[0].buf   = &outbuf;
+  /*
+   * In order to read a register, we first do a "dummy write" by writing
+   * 0 bytes to the register we want to read from.  This is similar to
+   * the packet in set_i2c_register, except it's 1 byte rather than 2.
+   */
+  outbuf = reg;
+  messages[0].addr = _i2c_address;
+  messages[0].flags = 0;
+  messages[0].len = sizeof(outbuf);
+  messages[0].buf = &outbuf;
 
-    /* The data will get returned in this structure */
-    messages[1].addr  = _i2c_address;
-    messages[1].flags = I2C_M_RD/* | I2C_M_NOSTART*/;
-    messages[1].len   = sizeof(inbuf);
-    messages[1].buf   = inbuf;
+  /* The data will get returned in this structure */
+  messages[1].addr = _i2c_address;
+  messages[1].flags = I2C_M_RD /* | I2C_M_NOSTART*/;
+  messages[1].len = sizeof(inbuf);
+  messages[1].buf = inbuf;
 
-    /* Send the request to the kernel and get the result back */
-    packets.msgs      = messages;
-    packets.nmsgs     = 2;
-    if(ioctl(i2C_file, I2C_RDWR, &packets) < 0) {
-        rt_printf("Unable to send data");
-        return 0;
-    }
+  /* Send the request to the kernel and get the result back */
+  packets.msgs = messages;
+  packets.nmsgs = 2;
+  if (ioctl(i2C_file, I2C_RDWR, &packets) < 0) {
+    rt_printf("Unable to send data");
+    return 0;
+  }
 
-    return (uint16_t)inbuf[0] | (((uint16_t)inbuf[1]) << 8);
+  return (uint16_t)inbuf[0] | (((uint16_t)inbuf[1]) << 8);
 }
 
 /**************************************************************************/
@@ -164,12 +162,10 @@ uint16_t I2C_MPR121::readRegister16(uint8_t reg) {
 */
 /**************************************************************************/
 void I2C_MPR121::writeRegister(uint8_t reg, uint8_t value) {
-	uint8_t buf[2] = { reg, value };
+  uint8_t buf[2] = {reg, value};
 
-	if(write(i2C_file, buf, 2) != 2)
-	{
-		std::cout << "Failed to write register " << (int)reg << " on MPR121\n";
-		return;
-	}
+  if (write(i2C_file, buf, 2) != 2) {
+    std::cout << "Failed to write register " << (int)reg << " on MPR121\n";
+    return;
+  }
 }
-
