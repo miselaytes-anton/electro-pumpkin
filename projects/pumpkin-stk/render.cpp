b8a44d58bd2201f30c542b59ed5acedb91f4b1f1
Anton Miselaytes <5480808+miselaytes-anton@users.noreply.github.com>
2020-04-03 22:18:52 +0200

    Replace STK freeverb with JCRev

--- projects/pumpkin-stk/render.cpp
+++ projects/pumpkin-stk/render.cpp
@@ -1,13 +1,11 @@
 #include <Bela.h>
 #include <stk/ADSR.h>
 #include <stk/BlitSaw.h>
-#include <stk/FreeVerb.h>
+#include <stk/JCRev.h>
 #include <stk/Generator.h>
 #include <stk/OnePole.h>
 #include <stk/SineWave.h>
-
 #include <cmath>
-
 #include "I2C_MPR121/I2C_MPR121.h"
 
 using namespace stk;
@@ -22,22 +20,26 @@ StkFloat gFrequencies[NUM_TOUCH_PINS] = {261.63, 293.66, 329.63, 349.23,
                                          587.33, 659.25, 698.25, 783.99};
 
 StkFrames frames;
+StkFrames envelopeFrames;
+StkFrames tmpFrames;
+
 BlitSaw *oscillators[NUM_TOUCH_PINS];
+StkFloat amplitudeThreshold = 0.01;
 
 StkFloat gAttack = 0.1;           // Envelope attack (seconds)
 StkFloat gDecay = 0.25;           // Envelope decay (seconds)
-StkFloat gRelease = 2;          // Envelope release (seconds)
+StkFloat gRelease = 0.5;          // Envelope release (seconds)
 StkFloat gSustain = 1.0;          // Envelope sustain level
 ADSR *envelopes[NUM_TOUCH_PINS];  // ADSR envelope
 
-OnePole *lpFilter;
-StkFloat lpFilterFc = 0;
+auto lowPassFilter = new OnePole{};
+StkFloat lowPassFilterFc = 400;
 
-SineWave *lfo;
-StkFloat lfoFreq = 0.5;
-int lfoDepth = 150;
+auto lfo = new SineWave{};
+StkFloat lfoFreq = 2;
+int lfoDepth = 300;
 
-FreeVerb *freeverb;
+auto reverb = new JCRev {};
 StkFloat volume = 0.5;
 
 /* MPR 121 */
@@ -54,10 +56,10 @@ void readMPR121(void *);
 
 void setEnvelopeGate(ADSR *envelope, float amplitude) {
   unsigned int envState = envelope->getState();
-  if (amplitude > 0.01 &&
+  if (amplitude > amplitudeThreshold &&
       (envState == ADSR::IDLE || envState == ADSR::RELEASE)) {
     envelope->keyOn();
-  } else if (amplitude < 0.01 && envState != ADSR::IDLE) {
+  } else if (amplitude < amplitudeThreshold && envState != ADSR::IDLE) {
     envelope->keyOff();
   }
 }
@@ -68,7 +70,17 @@ void setLowPass(StkFloat frequency, OnePole *lfo) {
   StkFloat b0 = 1.0f - a1;
 
   // b_[0] * inputs_[0] - a_[1] * outputs_[1];
-  lfo->setCoefficients(a1, b0);
+  lfo->setCoefficients(b0, a1);
+}
+
+void audioWriteFrames(BelaContext *context, StkFrames &frames) {
+  for (unsigned int frame = 0; frame < context->audioFrames; frame++) {
+    for (unsigned int channel = 0; channel < context->audioOutChannels;
+         channel++) {
+      float out = frames(frame, channel) * 0.1;
+      audioWrite(context, frame, channel, out);
+    }
+  }
 }
 
 bool setup(BelaContext *context, void *userData) {
@@ -82,6 +94,9 @@ bool setup(BelaContext *context, void *userData) {
 
   Stk::setSampleRate(context->audioSampleRate);
   frames.resize(context->audioFrames, context->audioOutChannels);
+  tmpFrames.resize(context->audioFrames, context->audioOutChannels);
+  envelopeFrames.resize(context->audioFrames, context->audioOutChannels);
+
   for (unsigned int i = 0; i < NUM_TOUCH_PINS; i++) {
     oscillators[i] = new BlitSaw{};
     oscillators[i]->setFrequency(gFrequencies[i] / 2);
@@ -95,16 +110,8 @@ bool setup(BelaContext *context, void *userData) {
     envelopes[i]->setSustainLevel(gSustain);
   }
 
-  lfo = new SineWave{};
   lfo->setFrequency(lfoFreq / context->audioSampleRate);
-  // setLowPass(lpFilterFc, lpFilter);
-
-  freeverb = new FreeVerb{};
-  freeverb->setMode(1);
-  freeverb->setEffectMix(0.1);
-  freeverb->setRoomSize(0.1);
-  freeverb->setDamping(0.1);
-  freeverb->setWidth(0.1);
+  setLowPass(lowPassFilterFc, lowPassFilter);
 
   if (context->analogFrames) {
     gAudioFramesPerAnalogFrame = context->audioFrames / context->analogFrames;
@@ -114,40 +121,36 @@ bool setup(BelaContext *context, void *userData) {
 }
 
 void render(BelaContext *context, void *userData) {
-  for (unsigned int n = 0; n < context->audioFrames; n++) {
-    // schedule touch sensor readings
-    if (++readCount >= readIntervalSamples) {
-      readCount = 0;
-      Bela_scheduleAuxiliaryTask(i2cTask);
-    }
-    lpFilterFc = map(analogRead(context, n / gAudioFramesPerAnalogFrame, 0), 0,
-                     0.8, 200, 2500);
-    // setLowPass((lpFilterFc + lfo->tick() *
-    // lfoDepth)/context->audioSampleRate, lpFilter);
+  Bela_scheduleAuxiliaryTask(i2cTask);
 
-    StkFloat sample = 0.0;
-
-    int numPressed = 1;
-    for (int i = 0; i < NUM_TOUCH_PINS; i++) {
-      if (i >= 4 && i <= 7) {
-        // sensor 4-7 are not connected, and we do not want to skip
-        // notes
-        continue;
-      }
-
-      float amplitude = sensorValue[i] / 400.f;
-
-      setEnvelopeGate(envelopes[i], amplitude);
-      if (envelopes[i]->getState() != ADSR::IDLE) {
-        numPressed++;
-      }
-      sample += envelopes[i]->tick() * oscillators[i]->tick();
-    }
-    StkFloat out = freeverb->tick(sample/ numPressed) ;
-    for (unsigned int ch = 0; ch < context->audioInChannels; ch++) {
-      context->audioOut[context->audioInChannels * n + ch] = out * volume;
-    }
+  for (int i = 0; i < frames.size(); i++) {
+    frames[i] = 0;
+    tmpFrames[i] = 0;
+    envelopeFrames[i] = 0;
   }
+
+  for (int i = 0; i < NUM_TOUCH_PINS; i++) {
+    float amplitude = sensorValue[i] / 400.f;
+    if (i >= 4 && i <= 7) {
+      // sensor 4-7 are not connected, and we do not want to skip
+      // notes
+      continue;
+    }
+    if (amplitude < amplitudeThreshold && envelopes[i]->getState() == ADSR::IDLE) {
+      continue;
+    }
+
+    setEnvelopeGate(envelopes[i], amplitude);
+
+    oscillators[i]->tick(tmpFrames);
+    envelopes[i]->tick(envelopeFrames);
+    tmpFrames *= envelopeFrames;
+    frames += tmpFrames;
+  }
+  lowPassFilter->tick(frames);
+  reverb->tick(frames);
+
+  audioWriteFrames(context, frames);
 }
 
 void cleanup(BelaContext *context, void *userData) {}
